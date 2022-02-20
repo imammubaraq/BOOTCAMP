691884aca8352a6d28354458ca55c315595f3870
imammubaraq <imammubaraq12@gmail.com>
2022-02-20 04:39:31 -0800

    update

---
+++ coba.php
@@ -0,0 +1,14 @@
+<?php
+
+$capitals = [
+	'mobil' => 'lambo',
+	'rumah' => 'indo',
+	'asli' => 'NTB',
+	'jogja' => 'grojogan',
+	'States' => 'menikah'
+];
+
+foreach ($capitals as $country => $capital) {
+	echo "The imam mubaraq!!!!{$country} is $capital" . '<br>';
+}
+?>
